602ca5738f5236056b7ee847679094fbfe4358d3
Cristian Mazilu <mazilu6@gmail.com>
2023-07-11 20:08:23 +0300

    Added the first actual checkpoint: navigation

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
-    try {
-      // Initialize Firebase
-      let app = firebase.app();
-      const db = firebase.firestore();
+  try {
+    // Initialize Firebase
+    let app = firebase.app();
+    const db = firebase.firestore();
+
+    // Form submission logic
+    const passcodeForm = document.querySelector('#passcodeForm');
+    const passcodeInput = document.querySelector('#passcodeInput');
+    const messageEl = document.querySelector('#message');
+
+    passcodeForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const passcode = passcodeInput.value;
+      const querySnapshot = await db.collection('checkpoints').where('passCode', '==', passcode).get();
+
+      if (!querySnapshot.empty) {
+        // Access the first document that matches the query
+        const doc = querySnapshot.docs[0];
+        window.location.href = doc.data().redirect;
+      } else {
+        messageEl.textContent = 'Invalid passcode \'' + passcodeInput.value + '\' . Please try again.';
+      }
+    });
+
+  } catch (e) {
+    console.error(e);
+    messageEl.textContent = 'Error initializing Firebase, check the console.';
+  }
+});
   
-      // Form submission logic
-      const passcodeForm = document.querySelector('#passcodeForm');
-      const passcodeInput = document.querySelector('#passcodeInput');
-      const messageEl = document.querySelector('#message');
-  
-      passcodeForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const passcode = passcodeInput.value;
-        const querySnapshot = await db.collection('checkpoints').where('passCode', '==', passcode).get();
-  
-        if (!querySnapshot.empty) {
-          // Access the first document that matches the query
-          const doc = querySnapshot.docs[0];
-          window.location.href = doc.data().redirect;
+
+document.addEventListener('DOMContentLoaded', function() {
+  try {
+    // Initialize Firebase
+    let app = firebase.app();
+    const db = firebase.firestore();
+
+    // Form submission logic
+    const solutionForm = document.querySelector('#solutionForm');
+    const solutionInput = document.querySelector('#solutionInput');
+    const solutionMessageEl = document.querySelector('#solutionMessage');
+    const mapDisplayEl = document.querySelector('#mapDisplay');
+
+    solutionForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const solution = solutionInput.value.trim().toLowerCase(); // also perform case-insensitive comparison
+      const querySnapshot = await db.collection('checkpoints').where('redirect', '==', window.location.href).get();
+
+      if (!querySnapshot.empty) {
+        // Access the first document that matches the query
+        const doc = querySnapshot.docs[0];
+        const dbSolution = doc.data().solution.toLowerCase(); // make it case-insensitive
+
+        if (solution === dbSolution) {
+          mapDisplayEl.style.display = 'block';
         } else {
-          messageEl.textContent = 'Invalid passcode \'' + passcodeInput.value + '\' . Please try again.';
+          solutionMessageEl.textContent = 'Mai încearcă!';
         }
-      });
-  
-    } catch (e) {
-      console.error(e);
-      messageEl.textContent = 'Error initializing Firebase, check the console.';
-    }
-  });
-  
+      } else {
+        solutionMessageEl.textContent = 'Se pare că ai încercat un checkpoint greșit. Încearcă din nou, sau contactează organizatorii.';
+      }
+    });
+
+  } catch (e) {
+    console.error(e);
+    solutionMessageEl.textContent = 'Eroare de accesare a bazei de date. Contactează organizatorii.';
+  }
+});
